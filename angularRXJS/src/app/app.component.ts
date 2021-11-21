fc66547371c42db66b04926d0552667bbedc69f0
GAX3 <gustavogaa01@gmail.com>
2021-11-20 22:15:49 -0600

    Tap

--- angularRXJS/src/app/app.component.ts
+++ angularRXJS/src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component, OnInit } from "@angular/core";
-import { pipe, of, from, fromEvent } from "rxjs";
-import { map, filter } from "rxjs/operators";
+import {  fromEvent } from "rxjs";
+import { tap } from "rxjs/operators";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-// Map And Filter
+// Tap -
 
 export class AppComponent {
   title = 'angularRXJS Map and Filter';
@@ -15,23 +15,16 @@ export class AppComponent {
   constructor (){}
 
   public ngOnInit(): void{    
-    const nums = of(1,2,3,4,5,6,7,8,9,10);
-  
-    const alCuadrado = pipe(
-      filter((n: number | any ) => n % 2 === 0),
-      map(n => n *n)
-    );
+   const clicks = fromEvent(document, 'click');
 
-    const cuadrado = alCuadrado(nums);
+   const position = clicks.pipe(
+    tap(ev => console.log(`Procesado ${ev}`),
+      err => console.log(err),
+      () => console.log('Completado')
+    )
+   );
 
-    cuadrado.subscribe((x)=>{
-      console.log(`Cuadrado: ${x}`)
-    })
-
-
-  
-  
-  
+   position.subscribe((pos) => console.log(pos));
 
   }
 
