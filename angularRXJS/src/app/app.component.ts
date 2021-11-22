1c6fc025450613b26d29d6ad6aab01b04d6ab58d
GAX3 <gustavogaa01@gmail.com>
2021-11-21 20:31:36 -0600

    Fork Join

--- angularRXJS/src/app/app.component.ts
+++ angularRXJS/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, ElementRef, OnInit, ViewChild } from "@angular/core";
-import { interval, fromEvent, from, merge, empty } from "rxjs";
-import { mapTo, scan, switchMap, takeWhile, startWith } from "rxjs/operators";
+import { interval, forkJoin, of } from "rxjs";
+import { delay, take } from "rxjs/operators";
+import { ajax } from 'rxjs/ajax'
 
 
 @Component({
@@ -8,45 +9,37 @@ import { mapTo, scan, switchMap, takeWhile, startWith } from "rxjs/operators";
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-// Buffer Time
+
 
 export class AppComponent {
-  title = 'angularRXJS switch Map ';
-
+  title = 'angularRXJS forkJoin';
 
   constructor(){}
-  /*
-  public ngOnInit(): void {
-    //One Line Nos permite Cambiar el estado de un mapTo 
-    const evento = fromEvent(document, 'click').pipe(switchMap ((val) =>  interval(500))).subscribe((x) => console.log("Valor: ", x));
 
-    //Two Line
-    //const evento = fromEvent(document, 'click').pipe(switchMap ((val) =>  interval(1000)));
-    //const resul = evento.subscribe((x) => console.log("Valor: ", x))
-  }
-  */
-
-  @ViewChild('pauseBtn', {static: true}) pauseBtn: ElementRef | undefined;
-  @ViewChild('resumeBtn', {static: true}) resumeBtn: ElementRef | undefined;
-  @ViewChild('remainingP', {static: true}) remainingP: ElementRef | undefined;
   public ngOnInit(): void{
-    const remainingLabel = document.getElementById('remaining');
+    /*
+    const fork = forkJoin(
+      of('Hola'),
+      of('Mundo').pipe(delay(500)),
+      interval(1000).pipe(take(2))
+    );
 
-    const obsInterval = interval(1000).pipe(mapTo(-1));
-    const pause = fromEvent(this.pauseBtn?.nativeElement, 'click').pipe(mapTo(false));
-    const resume = fromEvent(this.resumeBtn?.nativeElement, 'click').pipe(mapTo(true));
-
-    const timer = merge(pause, resume)
-      .pipe(
-        startWith(true),
-        switchMap(val => (val ? obsInterval : empty())),
-        scan((acc: any, curr: any) => (curr ? curr + acc : acc), 15),
-        takeWhile(v => v >=0 )       
-      )
-      //.subscribe((val: any) => (console.log(val)));
-      .subscribe((val) => (remainingLabel!.innerHTML = val));
-
+    fork.subscribe(val => console.log(val));
+    */
    
+    //Using Ajax
+    const src = forkJoin(
+      {
+        google: ajax.getJSON('https://api.github.com/users/google'),
+        microsoft: ajax.getJSON('https://api.github.com/users/microsoft'),
+        ctmil: ajax.getJSON('https://api.github.com/users/ctmil'),
+        gax3: ajax.getJSON('https://api.github.com/users/gax3')
+
+      }
+    );
+
+    src.subscribe((x) =>  console.log(x));
+
   }
 
 
